c7e5e18ddef5ebb193c8e21ae77cf7af2eb34b7a
aridepai17 <advaithdepai26@gmail.com>
2025-07-31 21:20:36 +0530

    feat(findmissingnumber.py): Implemented Find Missing Number

---
+++ 6 ARRAY EASY/findmissingnumber.py
@@ -0,0 +1,53 @@
+# FIND MISSING NUMBER 
+
+'''
+Given an integer array of size n containing distinct values in the range from 0 to n (inclusive), 
+return the only number missing from the array within this range.
+'''
+
+def missingNumber(nums):
+    xor1 = 0
+    xor2 = 0 
+    n = len(nums)
+    
+    for i in range(n):
+        xor1 ^= nums[i]
+        xor2 ^= i
+    
+    xor2 ^= n
+        
+    return xor1 ^ xor2
+
+# Examples 
+nums1 = [0, 2, 3, 1, 4]
+print(missingNumber(nums1)) # Output: 5
+
+nums2 = [0]  
+print(missingNumber(nums2)) # Output: 1
+
+nums3 = [1, 0]  
+print(missingNumber(nums3)) # Output: 2
+
+nums4 = [0, 1, 2, 3]  
+print(missingNumber(nums4)) # Output: 4
+
+nums5 = [0, 1, 3]  
+print(missingNumber(nums5)) # Output: 2
+
+nums6 = [1, 2, 3, 4, 5]  
+print(missingNumber(nums6)) # Output: 0
+
+nums7 = [0, 1, 2, 4, 5]  
+print(missingNumber(nums7)) # Output: 3
+
+nums8 = [0, 1, 3, 4, 5, 6]  
+print(missingNumber(nums8)) # Output: 2
+
+nums9 = [0, 1, 2, 3, 5]  
+print(missingNumber(nums9)) # Output: 4
+
+nums10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  
+print(missingNumber(nums10)) # Output: 10
+
+nums11 = [1, 2, 3, 4, 5, 6, 7, 8, 9]  
+print(missingNumber(nums11)) # Output: 0
