e174d5b18bbe9743ed6701065943080e2491a86e
aridepai17 <advaithdepai26@gmail.com>
2025-08-08 10:41:35 +0530

    feat(3sum.py): Implemented 3 Sum

--- 9 ARRAY HARD/3sum.py
+++ 9 ARRAY HARD/3sum.py
@@ -8,38 +8,36 @@ Notice that the solution set must not contain duplicate triplets.
 One element can be a part of multiple triplets. The output and the triplets can be returned in any order.
 '''
 
-def threeSum(self, nums):
-        nums.sort()
-        n = len(nums)
-        result = []
-
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-
-            left, right = i + 1, n - 1
-
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-
-                if total == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
-                    left += 1
-                    right -= 1
-
-                elif total < 0:
-                    left += 1
-                else:
-                    right -= 1
-
-        return result
+def threeSum(nums):
+    n = len(nums)
+    result = []
+    nums.sort()
     
+    for i in range(n):
+        if i != 0 and nums[i] == nums[i - 1]:
+            continue
+        
+        left = i + 1
+        right = n - 1
+        
+        while left < right:
+            totalSum = nums[i] + nums[left] + nums[right]
+            if totalSum < 0:
+                left += 1
+            elif totalSum > 0:
+                right -= 1
+            else:
+                result.append([nums[i], nums[left], nums[right]])
+                left += 1
+                right -= 1
+                
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+                    
+    return result
+        
 # Example 
 nums1 = [2, -2, 0, 3, -3, 5]
 print(threeSum(nums1)) # Output: [[-2, 0, 2], [-3, -2, 5], [-3, 0, 3]]
